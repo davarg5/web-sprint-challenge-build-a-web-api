5bf735975e0dc55e6b9532273dc66641b4396336
Daniel Vargas <davarg5@yahoo.com>
2020-12-03 19:24:50 -0500

    set up project

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,71 @@
 // Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+const Action = require('./actions-model');
+
+router.get('/', (req, res) => {
+    Action.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Error retrieving the actions' })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Action.get(id)
+        .then(action => {
+            if(!action) {
+                res.status(404).json({ message: 'There is no action with that id' })
+            }
+            res.status(200).json(action);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Error retrieving the action' })
+        })
+})
+
+
+// Start fixing here
+router.post('/', (req, res) => {
+
+    Action.insert(req.body)
+        .then(action => {
+            if(!action) {
+                res.status(404).json({ message: 'There are no projects with that id' })
+            }
+            res.status(201).json(action);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Error creating the action' })
+        })
+})
+
+router.put('/', (req, res) => {
+    Action.update(req.body.project_id, req.body)
+        .then(action => {
+            if(!action) {
+                res.status(404).json({ message: 'There are no projects with that id' })
+            }
+            res.status(200).json(action);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Error updating the action' })
+        })
+})
+
+router.delete('/', (req, res) => {
+
+    Action.remove()
+        .then(() => {
+
+            res.status(204);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Error deleting the action' })
+        })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -4,4 +4,17 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+server.use(express.json());
+
+const ActionRouter = require('./actions/actions-router');
+server.use('/api/actions', ActionRouter);
+
+// const ProjectRouter = require('./projects/projects-router');
+// server.use('/api/projects', ProjectRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Welcome to the Posts and Actions API</h2>`)
+})
+
+
 module.exports = server;
